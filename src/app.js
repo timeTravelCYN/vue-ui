d3e7f20798cf461e70b8322180220556030aadc1
timetravel <554793916@qq.com>
2018-08-25 10:52:19 +0800

    Svg 分离成 Icon.vue, 组件化代码

--- src/app.js
+++ src/app.js
@@ -1,7 +1,9 @@
 import Vue from 'vue'
 import Button from '../package/button'
+import Icon from '../package/common/icon'
 
 Vue.component('g-button', Button);
+Vue.component('g-icon', Icon)
 
 new Vue({
   el: '#app'
