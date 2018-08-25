348db20c25078924a3a780a082aa80315a1063e7
timetravel <554793916@qq.com>
2018-08-25 10:44:21 +0800

    验证 props

--- package/button.vue
+++ package/button.vue
@@ -12,7 +12,16 @@
 <script>
   export default {
     name: 'g-button',
-    props: ['icon', 'iconPosition']
+    props: {
+      icon: {},
+      iconPosition: {
+        type: String,
+        default: 'left',
+        validator: function (value) {
+          return !(value !== 'left' && value !== 'right') ? true : 'left'
+        }
+      }
+    }
   }
 </script>
 
